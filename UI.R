11e544fe6655ca385e1b3a491b1a68e70d5e43ad
shadrack kibet <shadrackkibet36@outlook.com>
2019-08-17 11:38:10 +0300

    code formatting

--- UI.R
+++ UI.R
@@ -1,20 +1,21 @@
 library(shiny)
 library(shinydashboard)
+
 shinyUI(
-  dashboardPage( title = "examsapp",skin = "blue",
-  dashboardHeader(title = "Exams App:",titleWidth = 300),
+  dashboardPage( title = "examsapp", skin = "blue",
+  dashboardHeader(title = "Examination Application:", titleWidth = 300),
   dashboardSidebar(width = 300,
     sidebarMenu(
       fileInput("data_file","Upload the file:"),
-      selectInput("subject","Select Subject:",choices = c("Language"="LANGUAGE","Composition"="COMPOSITION","Kiswahili"="LUGHA","Insha"="INSHA","Mathematics"="MATHEMATICS","Science"="SCIENCE","Social Studies"="S.S","Religious Education"="C.R.E"),selected = "LANGUAGE" )
+      selectInput("subject", "Select Subject:", choices = c("Language" = "LANGUAGE", "Composition" = "COMPOSITION", "Kiswahili" = "LUGHA", "Insha" = "INSHA", "Mathematics" = "MATHEMATICS", "Science" = "SCIENCE", "Social Studies" = "S.S", "Religious Education" = "C.R.E"), selected = "LANGUAGE" )
       )),
     dashboardBody(
       tabsetPanel(
-        type="tab",
-        tabPanel("Raw Data",downloadButton("raw_data","Download Raw Data"),
+        type = "tab",
+        tabPanel("Raw Data",downloadButton("raw_data", "Download Raw Data"),
                tableOutput("data_file")
                  ),
-        tabPanel("Tabulated Data",downloadButton("tabulated_data","Download Tabulated Data")
+        tabPanel("Tabulated Data", downloadButton("tabulated_data", "Download Tabulated Data")
                  
                  ),
         tabPanel("Summaries",
@@ -23,12 +24,12 @@ shinyUI(
         tabPanel("Plots",
                  fluidRow(
                    tabBox(
-                     tabPanel("Barchart", plotOutput("barchart"),icon = icon("bar-chart")),
-                     tabPanel("Histogram", plotOutput("histogram"),icon = icon("bar-chart"),
-                              sliderInput("bins","Number of breaks:",1,100,50)),
+                     tabPanel("Barchart", plotOutput("barchart"), icon = icon("bar-chart")),
+                     tabPanel("Histogram", plotOutput("histogram"), icon = icon("bar-chart"),
+                              sliderInput("bins","Number of breaks:", 1, 100, 50)),
                      tabPanel("Boxplot", plotOutput("boxplot")),
-                     tabPanel("Scatter plot", plotOutput("scatterplot")),
-                     downloadButton("plot","Download Plot")
+                     #tabPanel("Scatter plot", plotOutput("scatterplot")),
+                     downloadButton("plot", "Download Plot")
                    )
                    )
                  )
